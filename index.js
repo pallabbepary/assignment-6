7ba15d6ada040d8c0da301e091a89cce404bbe86
pallabbepary <palllabbepary@gmail.com>
2026-01-12 22:00:47 +0600

    commit

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ function displayCategories (data){
     // console.log(data);
 
     const LearnVocabulariesContainer = document.getElementById("LearnVocabularies-container");
+    // LearnVocabulariesContainer.innerHTML = ""
+    
     for(let cat of data){
         // console.log(cat)
         const categoriesDiv = document.createElement("div")
         categoriesDiv.innerHTML = `
-            <button class="btn font-semibold text-sm text-blue-700 border-blue-700 hover:bg-blue-700 hover:text-white"> <i class="fa-solid fa-book-open"></i> Lesson- ${cat.level_no}</button>
+            <button onclick = "loadLessonLevel(${cat.level_no})" class="btn font-semibold text-sm text-blue-700 border-blue-700 hover:bg-blue-700 hover:text-white"> <i class="fa-solid fa-book-open"></i> Lesson- ${cat.level_no}</button>
     
         `
         LearnVocabulariesContainer.append(categoriesDiv)
@@ -31,8 +33,20 @@ function loadLessonBtn(){
 
 const displayLesson = (data)=>{
     const lessonContainer = document.getElementById("lesson-Container")
+    lessonContainer.innerHTML = "";
+
+    if(data.length == 0){
+        lessonContainer.innerHTML = `
+            <div class="col-span-full flex flex-col justify-center items-center     text-center py-10">
+                    <img class="w-[140px]" src="./assets/alert-error.png" alt="">
+                    <h4 class="text-[#79716B] py-5">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</h4>
+                    <h1 class="font-medium text-4xl">নেক্সট Lesson এ যান</h1>
+                </div>
+        `;
+        return;
+    }
     data.forEach((lesson) =>{
-        console.log(lesson);
+        // console.log(lesson);*******************
         const lessonCard = document.createElement("div")
         lessonCard.innerHTML = `
             <div class="card bg-base-100 shadow-sm ">
@@ -64,4 +78,13 @@ const displayLesson = (data)=>{
 }
 
 
-loadLessonBtn()
+// loadLessonBtn()
+
+
+const loadLessonLevel = (level_no) =>{
+    const url = `https://openapi.programming-hero.com/api/level/${level_no}`;
+    // console.log(url)
+    fetch(url)
+    .then(res => res.json())
+    .then(data=>displayLesson(data.data))
+}
